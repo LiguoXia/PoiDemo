5fdc4e220520823ab05efdf0ad667b0afd67e773
夏利国 <xialiguo@tcl.com>
2018-01-29 11:28:03 +0800

    equals重写

--- src/main/java/com/xwq520/faceobject/TestEquals.java
+++ src/main/java/com/xwq520/faceobject/TestEquals.java
@@ -3,8 +3,17 @@ package com.xwq520.faceobject;
 public class TestEquals {
 	public static void main(String[] args) {
 		Cat c1 = new Cat(1, 2, 3);
-		Cat c2 = new Cat(1, 2, 4);
-		System.out.println(c1.equals(c2));
+		Cat c2 = new Cat(1, 2, 3);
+		System.out.println(c1.equals(c2));// true
+		System.out.println(c1 == c2);// false
+		String s1 = "123";
+		String s2 = "123";
+		String s3 = new String("123");
+		String s4 = new String("123");
+		System.out.println(s1 == s2);// true
+		System.out.println(s3 == s4);// false
+		System.out.println(s1 == s3);// false
+		System.out.println(s1 instanceof Object);//true，用来判断引用类型变量指向的对象是否属于该类或该类的子类，用来判断String是否是Object或者是Object的子类
 	}
 }
 
@@ -31,5 +40,10 @@ class Cat {
 			}
 		}
 		return false;
+	}
+
+	@Override
+	public String toString() {
+		return "Cat [color=" + color + ", height=" + height + ", weight=" + weight + "]";
 	}
 }
