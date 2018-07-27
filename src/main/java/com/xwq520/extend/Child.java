a62426e31db4185af11feb0ef719514db5cd77d1
xialiguo <xialiguo@tcljr.com>
2018-07-27 10:46:12 +0800

    继承、重写、向上转型

--- src/main/java/com/xwq520/extend/Child.java
+++ src/main/java/com/xwq520/extend/Child.java
@@ -1,31 +1,63 @@
 package com.xwq520.extend;
 
+public class Child extends Parent {
 
-public class Child extends Parent{
+	// 子类不能继承父类的构造器（构造方法或者构造函数），但是父类的构造器带有参数的，则必须在子类的构造器中显式地通过super关键字调用父类的构造器并配以适当的参数列表。
+	// 如果父类有无参构造器，则在子类的构造器中用super调用父类构造器不是必须的
+	// 如果没有使用super关键字，系统会自动调用父类的无参构造器。
 	
+	// super 表示使用它的类的父类。super 可用于：
+	// 调用父类的构造方法；
+	// 调用父类的方法（子类覆盖了父类的方法时）；
+	// 访问父类的数据域（可以这样用但没有必要这样用）。
+	// 调用父类的构造方法语法：
+	// super();
+	// 或
+	// super(参数列表);
 	public Child(int a, int b, int c, int d) {
 		super(a, b, c, d);
-		// TODO Auto-generated constructor stub
 	}
 
 	public static void main(String[] args) {
-		Child c = new Child(2,2,2,2);
-		Parent p = new Child(2,2,2,2);
-//		Child d = new Parent(); 类型不匹配：无法从父项转换为子项
-		c.hhh();
-		System.out.println(c.hh());
+		Child c = new Child(2, 2, 2, 2);
+		// Child d = new Parent(); 类型不匹配：无法从父项转换为子项
+		c.self();
+		
+		/*向上转型：父类引用指向子类对象*/
+		// 指向子类的父类引用由于向上转型了，它只能访问父类中拥有的方法和属性
+		// 若子类重写了父类中的某些方法，在调用该些方法的时候，必定是使用子类中定义的这些方法（动态连接、动态调用）。
+		// 父类类型的引用可以调用父类中定义的所有属性和方法，对于只存在与子类中的方法和属性它就望尘莫及了
+		Parent p = new Child(2, 2, 2, 2);
+		System.out.println(p.publicCan());
+		System.out.println(p.protectedCan());
+		System.out.println(p.defaultSomeCan());
+		
+		/*向下转型：指向子类对象的父类引用赋给子类引用，要强制转换*/
+		Child c1 = (Child) p;
+		
+		// 子类对象可以直接调用父类的方法
+		System.out.println(c.publicCan());
+		System.out.println(c.protectedCan());
+		System.out.println(c.defaultSomeCan());
 	}
 
-	public int hhh() {
+	public int self() {
 		int c = 4;
-		Child c1 = new Child(3,3,33,3);
+		Child c1 = new Child(3, 3, 33, 3);
 		System.out.println("子类");
 		System.out.println(c1.a);
-//		System.out.println(c1.b);   Parent.b字段不可见
+		// System.out.println(c1.b); Parent.b字段不可见
 		System.out.println(c1.c);
 		System.out.println(c1.d);
-		System.out.println("这是父类的c："+super.c);//super 指向父类对象的引用
+		System.out.println(c1.Ab);
+		System.out.println("这是父类的c：" + super.c);// super 指向父类对象的引用
 		System.out.println();
 		return 4;
+	}
+	
+	// 重写父类的方法
+	public String publicCan() {
+		System.out.println("这里是子类重写父类的publicCan()方法");
+		return "我重写了";
 	}
 }
--- src/main/java/com/xwq520/extend/Parent.java
+++ src/main/java/com/xwq520/extend/Parent.java
@@ -1,7 +1,19 @@
 package com.xwq520.extend;
 
 public class Parent {
-	
+	public int a = 1;
+	private int b = 2; // 当前类内部可访问
+	protected int c = 3; // 子类可访问
+	int d = 4; // 同一个包中可访问，同一个 包中的子类可访问，不同包中的子类不可访问
+	public static final String Ab = "1";
+
+	// this 关键字
+	// this 关键字表示当前对象。可用于：
+	// 调用当前类的构造方法，并且必须是方法的第一条语句。如：this(); 调用默认构造方法。this(参数); 调用带参构造方法。
+	// 限定当前对象的数据域变量。一般用于方法内的局部变量与对象的数据域变量同名的情况。如 this.num = num。this.num
+	// 表示当前对象的数据域变量 num，而 num 表示方法中的局部变量。
+	// super 语句必须是子类构造方法的第一条语句。
+	// 调用父类的构造方法的唯一途径是使用 super 关键字，如果子类中没显式调用，则编译器自动将 super(); 作为子类构造方法的第一条语句。
 	public Parent(int a, int b, int c, int d) {
 		super();
 		this.a = a;
@@ -9,14 +21,34 @@ public class Parent {
 		this.c = c;
 		this.d = d;
 	}
-	public Parent(){
+	
+	// 若父类没有无参构造器，则必须在子类的构造器中显式地通过super关键字调用父类的构造器并配以适当的参数列表。
+	public Parent() {
+		super();
 	}
-	public int a = 1;
-	private int b = 2;  //当前类内部可访问
-	protected int c = 3; //子类可访问
-	int d = 4; //同一个包中可访问，同一个 包中的子类可访问，不同包中的子类不可访问
-	public int hh(){
-		System.out.println("这里是父类的方法");
-		return 3;
+
+	// 对所有类可见。使用对象：类、接口、变量、方法
+	public String publicCan() {
+		System.out.println("这里是父类的publicCan()方法");
+		return "父类public方法子类都可访问";
+	}
+
+	// 在同一包内可见，不使用任何修饰符。使用对象：类、接口、变量、方法。
+	// 子类若不在同一个包中，访问不了
+	String defaultSomeCan() {
+		System.out.println("这里是父类的defaultSomeCan()方法");
+		return "父类默认方法在同一个包中的子类才能访问";
+	}
+
+	// 在同一类内可见。使用对象：变量、方法。 注意：不能修饰类（外部类）
+	private String privateCannot() {
+		System.out.println("这里是父类的privateCannot()方法");
+		return "父类的private方法子类不能访问";
+	}
+
+	// 对同一包内的类和所有子类可见。使用对象：变量、方法。 注意：不能修饰类（外部类）。
+	protected String protectedCan() {
+		System.out.println("这里是父类的protectedCan()方法");
+		return "父类protected方法同一个包中和子类中可用";
 	}
 }
